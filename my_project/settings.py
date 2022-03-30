21cab2af599941e9babb1a0a436f0925c967053b
owolabi-develop <owolabidevelop84@gmail.com>
2022-03-30 14:09:27 -0700

    add security

--- my_project/settings.py
+++ my_project/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
