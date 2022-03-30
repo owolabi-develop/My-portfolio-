b7d93751351d749ab7d0c7288ce6234a2649d317
owolabi-develop <owolabidevelop84@gmail.com>
2022-03-30 14:22:02 -0700

    add email settinngs

--- my_project/settings.py
+++ my_project/settings.py
@@ -127,6 +127,12 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
