60aea1807bf2937587fd5fed251c0418000573fc
owolabi-develop <owolabidevelop84@gmail.com>
2022-03-30 12:51:46 -0700

    add static location

--- my_project/urls.py
+++ my_project/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('PortFolio/',include('PortFolio.urls')),
+    path('',include('PortFolio.urls')),
 ]
