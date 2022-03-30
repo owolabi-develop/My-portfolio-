b6f76c6321edb1a9aff9870a39f450abdff505a4
owolabi-develop <owolabidevelop84@gmail.com>
2022-03-30 12:21:59 -0700

    correct router

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['developewithowolabi.herokuapp.com']
+ALLOWED_HOSTS = ['https://developewithowolabi.herokuapp.com']
 
 
 # Application definition
