3ae68eb4cc97b50bb028b71dcbbb6ac5f7c8b8e8
owolabi-develop <owolabidevelop84@gmail.com>
2022-03-31 06:24:04 -0700

    add scroll to view effect

--- PortFolio/static/PortFolio/main.js
+++ PortFolio/static/PortFolio/main.js
@@ -14,3 +14,26 @@ function NavBar(){
     var navElement = document.querySelector(".navDropDown");
     navElement.classList.toggle("navDropDown1")
 }
+window.onscroll = function(){ScrollAnimation()}
+function ScrollAnimation(){
+    const viewPorst = document.documentElement.scrollTop;
+     var service = document.getElementsByClassName("service")
+     var AboutMe = document.getElementsByClassName("AboutMe")
+     var Skills = document.getElementsByClassName("Skills")
+     var forms = document.getElementsByClassName("forms")
+     const effectElement = [forms,service,AboutMe,Skills];
+
+    effectElement.forEach((item)=>{
+        for(var i=0;i<item.length;i++){
+            if(viewPorst >= item[i].offsetTop){
+                item[i].classList.add("scrollEffect")
+            }
+        }
+    })
+    
+
+
+          
+}
+
+
